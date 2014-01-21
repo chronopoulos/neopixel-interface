d7e4e43567c82ec72fa171d6f86c3ad00b7989de
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2014-01-20 23:51:13 -0700

    added camera flash effect

--- animateGaussian.py
+++ animateGaussian.py
@@ -1,4 +1,4 @@
-import serial, time
+import serial, time, random
 import numpy as np
 
 arduino = serial.Serial('/dev/ttyUSB0', 57600)
@@ -10,9 +10,15 @@ while True:
     print c
     y = np.exp(-(x-c)**2/0.5)
     for i in range(60):
-        arduino.write(chr(0)) # r
-        arduino.write(chr(0))   # g
-        arduino.write(chr(int(y[i]*255)))   # b
+        if random.random() < 0.01:
+            # "camera flash" effect
+            arduino.write(chr(255)) # r
+            arduino.write(chr(255))   # g
+            arduino.write(chr(255))   # b
+        else:
+            arduino.write(chr(0)) # r
+            arduino.write(chr(0))   # g
+            arduino.write(chr(int(y[i]*255)))   # b
     c = c + 1
     if c>10.:
         c = -10.
