a40c1f0a336c7c02c428c8db5a019b7cfcf78333
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2014-01-21 18:49:51 -0700

    added clearLEDs.py

---
+++ clearLEDs.py
@@ -0,0 +1,8 @@
+import serial
+
+arduino = serial.Serial('/dev/ttyUSB0', 14400)
+
+for i in range(60):
+    arduino.write(chr(0)) # r
+    arduino.write(chr(0))   # g
+    arduino.write(chr(0))   # b
